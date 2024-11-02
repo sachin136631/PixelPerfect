b781eff88c7d33278206abd6b4e0ddb849f2cddc
Durga Sumesh <durgasumesh17@gmail.com>
2024-11-02 19:25:07 +0530

    add gen ai integration

---
+++ gemini.js
@@ -0,0 +1,12 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "name of top 10 dog breeds";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
